dbb151414fc4ce4f3cc30974abb4b6e62d19eb1c
Johary <joharyandrianjafimanohisolo@gmail.com>
2023-09-24 15:19:20 +0300

    petite correction

--- src/components/utils/BreadcrumbComponent.vue
+++ src/components/utils/BreadcrumbComponent.vue
@@ -1,22 +1,32 @@
 <template>
-<nav class="flex" aria-label="Breadcrumb">
-  <ol class="inline-flex items-center space-x-1 md:space-x-3">
-    <li class="inline-flex items-center" @click="$router.push('/home')">
-      <a class="inline-flex items-center text-sm font-medium text-gray-700 dark:text-gray-400">
-        <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
-        Home
-      </a>
-    </li>
-    <li v-for="item in data" @click="goback(item.path)">
-      <div class="flex items-center" :class="item.isCurrent?'current__url':''">
-        <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-        <a class="ml-1 text-sm font-medium text-gray-700 hover:text-danger-600 md:ml-2 dark:text-gray-400">
-            {{ item.title }}
+  <nav class="flex" aria-label="Breadcrumb">
+    <ol class="inline-flex items-center space-x-1 md:space-x-3">
+      <li class="inline-flex items-center" @click="$router.push('/home')">
+        <a class="inline-flex items-center text-sm font-medium text-gray-700 dark:text-gray-400">
+          <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z">
+            </path>
+          </svg>
+          Home
         </a>
-      </div>
-    </li>
-  </ol>
-</nav>
+      </li>
+      <li v-for="item in data" :key="item.title" @click="goback(item.path)">
+        <div class="flex items-center" :class="item.isCurrent ? 'current__url' : ''">
+          <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"></path>
+          </svg>
+          <a class="ml-1 text-sm font-medium text-gray-700 hover:text-danger-600 md:ml-2 dark:text-gray-400">
+            {{ item.title }}
+          </a>
+        </div>
+      </li>
+    </ol>
+  </nav>
 </template>
 
 <script setup>
@@ -25,23 +35,23 @@ import { useAppStore } from '@Stores/index.js';
 
 const router = useRouter();
 const appStore = useAppStore();
-const props= defineProps({
-    data:{
-        type: Array,
-        required: false,
-        default: [
-            {
-                title: "Home",
-                path: "/home",
-                isCurrent: true,
-            },
-        ]
-    }
+const props = defineProps({
+  data: {
+    type: Array,
+    required: false,
+    default: [
+      {
+        title: "Home",
+        path: "/home",
+        isCurrent: true,
+      },
+    ]
+  }
 })
 
-const goback = (path)=>{
+const goback = (path) => {
   appStore.isLoading = true;
-  setTimeout(()=>{
+  setTimeout(() => {
     router.push(path);
   }, 100)
 }
@@ -52,20 +62,19 @@ const goback = (path)=>{
 @tailwind components;
 @tailwind utilities;
 
-.current__url a{
-    color: grey;
+.current__url a {
+  color: grey;
 }
 
-.current__url a:hover{
-    color: grey;
+.current__url a:hover {
+  color: grey;
 }
 
-a{
-    transform: var(--transition);
-}
-a:hover{
-    color: var(--color-danger);
-    cursor: pointer;
+a {
+  transform: var(--transition);
 }
 
-</style>
+a:hover {
+  color: var(--color-danger);
+  cursor: pointer;
+}</style>
