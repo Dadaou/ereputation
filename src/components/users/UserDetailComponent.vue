79424703b192c113015e3a5849a0af6d899fc8f3
HasimamyAndria <hasimamyandriamitantsoa@gmail.com>
2023-10-31 18:40:44 +0300

    Update UserDetailComponent

--- src/components/users/UserDetailComponent.vue
+++ src/components/users/UserDetailComponent.vue
@@ -13,166 +13,155 @@
             </div>
         </div>
         <div class="personal__info border__bottom">
-            <div class="info__title">
+            <div v-if="!editing" class="info__title">
                 Name
             </div>
             <div class="info__content">
                 <div class="info__container">
-                    <span v-if="!enableEdit.name">{{ userStore.user.firstname }} {{ userStore.user.lastname }}</span>
-                    <div v-else class="info__edit">
-                        <div class="form__input">
-                            <div class="label">Firstname (s)</div>
-                            <input type="text" name="firstname" v-model="user.firstname">
+                    <div class="info__edit">
+                        <span v-if="!editing" >{{ userStore.user.firstname }} {{ userStore.user.lastname }}</span>
+                        <div v-else>
+                        <div class="grid gap-6 mb-6 md:grid-cols-2">
+                            <div>
+                                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name <span>*</span></label>
+                                <input type="text" id="first_name" v-model="user.firstname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm w-full p-2">
+                            </div>
+                            <div>
+                                <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name <span>*</span></label>
+                                <input type="text" id="last_name" v-model="user.lastname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm w-full p-2">
+                            </div>
                         </div>
-                        <div class="form__input">
-                            <div class="label">Lastname (s)</div>
-                            <input type="text" name="lastname" v-model="user.lastname">
                         </div>
-                    </div>
-                </div>
-                <div class="edit__actions">
-                    <div class="edit__actions">
-                        <span class="cancel" v-if="enableEdit.name" @click="enableEdit.name = false">
-                            Cancel
-                        </span>
-                        <span class="edit" @click="enableEdit.name = true">
-                            <i class="uil uil-edit"></i>
-                            Edit
-                        </span>
-                    </div>
+                    </div>        
                 </div>
             </div>
         </div>
-        <div class="personal__info border__bottom">
-            <div class="info__title">
-                Email Address
+        <div class="personal__info">
+            <div v-if="!editing" class="info__title">
+                Email
             </div>
             <div class="info__content">
                 <div class="info__container">
                     <div class="info__edit">
-                        <span v-if="!enableEdit.email">{{ userStore.user.email }}</span>
-                        <p v-if="!enableEdit.email">This is the email address you use to sign in. It’s also where we send
-                            you all confirmations.</p>
-                        <div v-else class="form__input">
-                            <div class="label">Email Address</div>
-                            <input type="email" name="email" v-model="user.email" required>
+                        <span v-if="!editing">{{ userStore.user.email }}</span>
+                        <p v-if="!editing"></p>
+                        <div v-else>
+                            <div class="grid gap-6 mb-6 md:grid-cols-2">
+                                <div>
+                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email Address <span>*</span></label>
+                                    <input type="email" id="email" v-model="user.email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm w-full p-2" required>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="edit__actions">
-                    <div class="edit__actions">
-                        <span class="cancel" v-if="enableEdit.email" @click="enableEdit.email = false">
-                            Cancel
-                        </span>
-                        <span class="edit" @click="enableEdit.email = true">
-                            <i class="uil uil-edit"></i>
-                            Edit
-                        </span>
-                    </div>
-                </div>
             </div>
+            
         </div>
-        <div class="personal__info border__bottom">
-            <div class="info__title">
-                Phone Number
+
+        <div class="personal__info">
+            <div v-if="!editing" class="info__title">
+               Phone number
             </div>
             <div class="info__content">
                 <div class="info__container">
-                    <div class="info__edit phone__number">
-                        <span v-if="!enableEdit.phone">Add Your Phone Number</span>
-                        <p v-if="!enableEdit.phone">Allows us to contact you directly.</p>
-                        <div v-else class="form__input">
-                            <div class="label">Phone number</div>
-                            <vue-country-code @onSelect="onSelect" :enabledPhoneNumbers="true">
-                            </vue-country-code>
+                    <div class="info__edit">
+                        <span v-if="!editing">Add your phone number</span>
+                        <p v-if="!editing">Allows us to contact you directly.</p>
+                        <div v-else>
+                            <div class="grid gap-6 mb-6 md:grid-cols-2"> 
+                                <div>                            
+                                <label for="date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Add your phone number <span>*</span></label>
+                                    <vue-country-code style="width: 100px;" @onSelect="onSelect" :enabledPhoneNumbers="true">
+                                    </vue-country-code>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="edit__actions">
-                    <div class="edit__actions">
-                        <span class="cancel" v-if="enableEdit.phone" @click="enableEdit.phone = false">
-                            Cancel
-                        </span>
-                        <span class="edit" @click="enableEdit.phone = true">
-                            <i class="uil uil-edit"></i>
-                            Edit
-                        </span>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="personal__info border__bottom">
-            <div class="info__title">
+        <div class="personal__info">
+            <div v-if="!editing" class="info__title">
                 Date of Birth
             </div>
             <div class="info__content">
                 <div class="info__container">
-                    <div class="info__edit date__birth">
-                        <span v-if="!enableEdit.birth">Date of birth</span>
-                        <p v-if="!enableEdit.birth">Enter Your date of Birth</p>
-                        <div v-else class="form___input">
-                            <div class="label">Date of birth</div>
-                            <!-- <VueDatePicker v-model="date" :flow="flow" /> -->
-                             <el-date-picker
-                                v-model="date"
-                                :size="'large'"
-                              />
+                    <div class="info__edit">
+                        <span v-if="!editing">Date of birth</span>
+                        <p v-if="!editing">Enter Your date of Birth</p>
+                        <div v-else>
+                            <div class="grid gap-6 mb-6 md:grid-cols-2">
+                                <div>
+                                    <label for="date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date of birth <span>*</span></label>
+                                   <!-- <VueDatePicker v-model="date" :flow="flow" /> -->
+                                        <el-date-picker
+                                            v-model="date"
+                                            :size="'large'"
+                                        />
+                                   </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="edit__actions">
-                    <span class="cancel" v-if="enableEdit.birth" @click="enableEdit.birth = false">
-                        Cancel
-                    </span>
-                    <span class="edit" @click="enableEdit.birth = true">
-                        <i class="uil uil-edit"></i>
-                        Edit
-                    </span>
-                </div>
             </div>
         </div>
-        <div class="personal__info border__bottom">
-            <div class="info__title">
+        <div class="personal__info">
+            <div v-if="!editing" class="info__title">
                 Address
             </div>
             <div class="info__content">
                 <div class="info__container">
                     <div class="info__edit">
-                        <span v-if="!enableEdit.address">Address</span>
-                        <p v-if="!enableEdit.address">Add your address</p>
-                        <div v-if="enableEdit.address" class="form__input">
-                            <div class="label">Address</div>
-                            <input type="text" name="address" placeholder="Your Street name and house/apartment number">
-                        </div>
-                        <div v-if="enableEdit.address" class="multiples__input">
-                            <div class="form__input">
-                                <div class="label">Town/City</div>
-                                <input type="city" name="city" required>
-                            </div>
-                            <div class="form__input">
-                                <div class="label">PostCode</div>
-                                <input name="zip" type="text" inputmode="numeric"
-                                    pattern="^(?(^00000(|-0000))|(\d{5}(|-\d{4})))$" required>
+                        <span v-if="!editing">Address</span>
+                        <p v-if="!editing">Add your address</p>
+                        <div v-if="editing">
+                            <div class="grid gap-6 mb-6 md:grid-cols-2">
+                                <div>
+                                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Adress <span>*</span></label>
+                                    <input type="text" name="address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm w-full p-2" placeholder="Your Street name and house/apartment number">
+                                </div>
                             </div>
                         </div>
-                        <div v-if="enableEdit.address" class="form__input">
-                            <div class="label">Country</div>
-                            <vue-country-code v-if="enableEdit.address" @onSelect="onSelect">
-                            </vue-country-code>
+                        
+                        <div v-if="editing" class="multiples__input">
+                            <div class="grid gap-6 mb-6 md:grid-cols-2">
+                            <div>
+                                <label for="city" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Town/City <span>*</span></label>
+                                <input type="city" name="city" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm w-full p-2">
+                            </div>
+                            <div>
+                                <label for="postCode" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">PostCode <span>*</span></label>
+                                <input name="zip" type="text" id="last_name"  
+                                    inputmode="numeric"
+                                    pattern="^(?(^00000(|-0000))|(\d{5}(|-\d{4})))$"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm w-full p-2">
+                            </div>
+                        </div>
+                        </div>
+                        <div v-if="editing">
+                            <div class="grid gap-6 mb-6 md:grid-cols-2">
+                                <div>
+                                    <label for="country" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country <span>*</span></label>
+                                    <vue-country-code  v-if="editing" @onSelect="onSelect">
+                                    </vue-country-code>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="edit__actions">
-                    <span class="cancel" v-if="enableEdit.address" @click="enableEdit.address = false">
+            </div>
+        </div>
+
+        <div class="edit__actions">
+                    <span class="cancel" v-if="editing" @click="editing = false">
                         Cancel
                     </span>
-                    <span class="edit" @click="enableEdit.address = true">
+                    <span class="edit" @click="editing = true">
                         <i class="uil uil-edit"></i>
                         Edit
                     </span>
                 </div>
-            </div>
-        </div>
     </div>
 </template>
 
@@ -189,6 +178,7 @@ const date = ref();
 const flow = ref(['month', 'year', 'calendar']);
 const userStore = useUserStore();
 
+
 let enableEdit = ref({
     name: false,
     email: false,
@@ -196,6 +186,9 @@ let enableEdit = ref({
     birth: false,
     address: false,
 });
+
+
+let editing = ref(false);
 
 let user = ref({
     firstname: '',
@@ -212,6 +205,10 @@ onBeforeMount(() => {
     user.value.address = userStore.user.address;
     console.log(userStore.user)
 });
+
+function toggleEdit() {
+    editing.value = !editing.value;
+}
 </script>
 
 <style scoped>
@@ -227,6 +224,7 @@ onBeforeMount(() => {
     display: flex;
     justify-content: space-between;
 }
+
 
 .profile__header h4 {
     color: var(--color-bg2);
@@ -263,7 +261,6 @@ onBeforeMount(() => {
     width: 100%;
     overflow: auto;
 }
-
 .info__content {
     display: flex;
     justify-content: space-between;
@@ -274,10 +271,43 @@ onBeforeMount(() => {
     width: 20%;
 }
 
+@media screen and (max-width: 800px) {
+  .user__main__container {
+    width: 120%;  }
+    .personal__info {
+        align-items: flex-start; /* Aligner les éléments en haut de la colonne */
+    }
+}
+input, select{
+    border-radius: 4px !important;
+    background-color: white;
+}
+
+label {
+    font-weight: 500 !important;
+    font-size: 14px !important;
+    color: var(--color-bg2) !important;
+}
+
+label span{
+    color: red;
+}
+input{
+    caret-color: var(--light-color-bg2);
+}
+.info__edit input {
+    width: 100%;
+}
+.info__content {
+    display: flex;
+    flex-direction: column; /* Empiler les éléments verticalement */
+    justify-content: flex-start; /* Aligner le contenu en haut de la colonne */
+    
+}
 .edit__actions {
     display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+    align-items: baseline; /* Aligner les éléments verticalement au centre */
+    gap: 10px; /* Espacement entre les éléments */
 }
 
 .edit__actions .edit {
@@ -299,38 +329,4 @@ onBeforeMount(() => {
     color: var(--color-white);
 }
 
-.info__edit {
-    min-width: 200px;
-}
-
-.info__edit input {
-    width: 100%;
-}
-
-.info__edit .label {
-    color: var(--color-black);
-    font-weight: 600;
-}
-
-.form___input {
-    margin: 5px;
-}
-
-.form__input {
-    margin: 5px;
-}
-
-.info__content span,
-p {
-    font-size: 14px;
-}
-
-.info__content span {
-    font-weight: 500;
-}
-@media screen and (max-width: 800px) {
-  .user__main__container {
-    width: 120%; /* Occuper toute la largeur sur les petits écrans */
-  }
-}
 </style>
