56076bd6a7efe9cef481e6cb1377724b468ddf83
HasimamyAndria <hasimamyandriamitantsoa@gmail.com>
2023-12-08 18:25:20 +0300

    Ajout fonctionnalité Advantage

---
+++ src/components/advantage/AdvantageFormComponent.vue
@@ -0,0 +1,446 @@
+<template>
+<div class="security__header border__bottom my-10">
+            <div class="security__edit">
+                <h4><i class="uil uil-calender"></i> Advantage</h4>
+                <p>Complete the following information to add an advantage.</p>
+            </div>
+        </div>
+        <div>
+            <form @submit.prevent="submit" @keydown.enter.prevent="submit" class="mt-4 px-2 h-full">
+                  
+                    <div class="grid gap-6 mb-6 md:grid-cols-2">
+                        <div>
+                            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Establishment <span></span></label>
+                            <el-select v-model="establishments" placeholder="Choose establishment" size="large" multiple collapse-tags collapse-tags-tooltip>
+                                <el-option
+                                v-for="item in userStore.user.customer.establishments"
+                                :key="item.id"
+                                :label="item.name"
+                                :value="`/api/${companiesStore.entity}/${item.id}`"
+                                />
+                            </el-select>
+                        </div>
+                        <div>
+                            <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category <span></span></label>
+                            <input type="text" id="category" v-model="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm w-full p-2">
+                        </div>
+                    </div>
+                    
+                    <div class="grid gap-6 mb-6 md:grid-cols-2">
+                        <div>
+                            <label for="scope" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Scope <span></span></label>
+                            <el-select v-model="scope" placeholder="Select scope" size="large">
+                                <el-option
+                                v-for="scope in scopeOptions"
+                                :key="scope.value"
+                                :label="scope.label"
+                                :value="scope.value"
+                                />
+                            </el-select>
+                        </div>
+                        <div>
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name <span></span></label>
+                            <input type="text" id="name" v-model="advantageName" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm w-full p-2">
+                        </div>
+                        
+                    </div>
+                    <div class="grid gap-6 mb-6 md:grid-cols-2">
+                        <div>
+                            <label for="amount" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount <span></span></label>
+                            <input type="number" id="amount" v-model="amount" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm w-full p-2">
+                        </div>
+                        <div>
+                            <label for="metric" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Metric <span></span></label>
+                            <input type="text" id="metric" v-model="metric" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm w-full p-2">
+                        </div>
+                        
+                    </div>
+                    <div class="grid gap-6 mb-6 md:grid-cols-2">
+                       
+                        <div>
+                            <label for="validity" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Validity <span></span></label>
+                            <input type="number" id="validity" v-model="validity" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm w-full p-2">
+                        </div>
+                        <div>
+                            <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Expired at <span></span></label>
+                            <el-date-picker
+                                v-model="dateTo"
+                                id="dateTo"
+                                :size="'large'"
+                              />
+                        </div>      
+                    </div>
+                  
+                    <div class="flex items-center justify-between px-3 py-2 border-t border-b dark:border-gray-600">
+                       <button type="submit" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+                                <SpinnerComponent :show-spinner="showSpinner" :color="'gray'"/> <span v-if="showSpinner">Loading ...</span>
+                               <span v-show="!showSpinner"><i class="uil uil-save"></i> {{ type }} advantage</span>
+                 </button>
+                        </div>
+                </form>
+        </div>
+</template>
+<script setup>
+import moment from 'moment';
+import { ref, inject, watch } from 'vue';
+import services from '@Services/services.js';
+import { useUserStore } from "@Stores/user.js";
+import { useEventStore } from "@Stores/event.js";
+import { useCompanyStore } from "@Stores/company.js";
+import { useAppStore } from "@Stores/app.js";
+import SpinnerComponent from '@Components/utils/SpinnerComponent.vue';
+import { ElMessage, ElOption, ElSelect, ElDatePicker } from 'element-plus';
+import 'element-plus/es/components/message/style/css'
+import 'element-plus/es/components/option/style/css'
+import 'element-plus/es/components/select/style/css'
+import 'element-plus/es/components/date-picker/style/css'
+
+const selectedScope = ref(''); // Valeur sélectionnée dans le menu déroulant
+const scopeOptions = ref([
+  { label: 'individual', value: 'individual' },
+  { label: 'bill', value: 'bill' },
+]);
+
+const companiesStore = useCompanyStore();
+const eventStore = useEventStore();
+const userStore = useUserStore();
+const appStore = useAppStore();
+const format = (date) => {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  return `${year}/${month}/${day}`;
+}
+const showSpinner = ref(false);
+/**
+ * Event
+ */
+ const dateFrom = ref(null);
+ const dateTo = ref(null);
+ const category = ref('');
+ const advantageName = ref('');
+ const eventName = ref('');
+ const establishments = ref([]);
+ const code = ref('');
+ const amount = ref('');
+ const metric = ref('');
+ const scope = ref(null);
+ const validity = ref('');
+
+ const type = ref('add');
+ const advantage_to_update = inject('advantage_to_update');
+ const advantages = inject('advantages');
+const activeAdvantageTab = inject('advantage_activeTab');
+const handleAdvantageAdded = (newAdvantage) => {
+    // Mettez à jour la liste des avantages dans le composant parent
+    advantages.value.push(newAdvantage);
+};
+watch(advantage_to_update, ()=>{
+    if(advantage_to_update.value != null){  
+    console.log(advantage_to_update.value) 
+        // dateFrom.value = new Date(advantage_to_update.value["datefrom"]);
+        dateTo.value = new Date(advantage_to_update.value["dateTo"]);
+        category.value = advantage_to_update.value["category"];
+        code.value = advantage_to_update.value["code"];
+        metric.value = advantage_to_update.value["metric"];
+        scope.value = advantage_to_update.value["scope"];
+        validity.value = advantage_to_update.value["validity"];
+        amount.value = advantage_to_update.value["amount"];
+        advantageName.value = advantage_to_update.value["name"];
+        establishments.value = advantage_to_update.value['establishment']
+        type.value = 'edit';
+    }
+})
+const getEstablishment = (data)=>{
+  let result = [];
+  console.log(data);
+   data.forEach(item=>{
+     companiesStore.establishments.forEach((element, index) => {
+         if(`/api/establishments/${element.id}` == item){
+                     result.push(element);
+         }         
+    })
+   })
+ return result;
+}
+
+const getEstablishmentsName = (data) => {
+    let establishments = userStore.user.customer != null ? userStore.user.customer.establishments : [];
+    let names = '';
+
+    if (Array.isArray(data)) {
+        data.forEach(item => {
+            establishments.forEach(establishment => {
+                if (item == `/api/establishments/${establishment.id}`) {
+                    if (names != '') {
+                        names = `${names}, ${establishment.name}`;
+                    } else {
+                        names = `${names} ${establishment.name}`;
+                    }
+                }
+            })
+        });
+    }
+
+    return names;
+}
+
+
+const getUris = (establishments)=>{
+  let uris = [];
+  establishments.forEach(establishment=>{
+    uris.push(establishment.competitor_tag) 
+  })
+  return uris;
+}
+
+const loadData = (_advantage, advantage)=>{
+  const new_advantage = {
+          id: _advantage.id,
+          name: _advantage.name,
+          category: _advantage.category,
+          code: _advantage.code,     
+          amount: _advantage.amount,
+          validity: _advantage.validity,
+          metric: _advantage.metric,           
+          scope: _advantage.scope,                           
+          dateTo: moment(_advantage.dateTo).toISOString(),
+          establishment_name :  getEstablishmentsName(_advantage.establishment), 
+  }
+  advantages.value.push(new_event);
+}
+
+const updateData = (_advantage)=>{
+     const new_advantage = {
+          id: _advantage.id,
+          name: _advantage.name,
+          category: _advantage.category,
+          amount: _advantage.amount,
+          code: _advantage.code,  
+          validity: _advantage.validity,
+          metric: _advantage.metric,           
+          scope: _advantage.scope,                           
+          expiredAt: moment(_advantage.dateTo).toISOString(),
+          establishment_name :  getEstablishmentsName(_advantage.establishment),
+          establishment: _event.establishment,
+      }
+
+      console.log(new_advantage);
+      advantages.value.forEach((advantage, index)=>{
+        if(advantage.id == new_advantage.id) advantages.value[index] = new_advantage;
+     })
+  }
+
+
+  const submit = async () => {
+    const advantageData = {
+        "category": category.value,
+        "code": code.value,
+        "name": advantageName.value,
+        "amount": amount.value,
+        "metric": metric.value,
+        "scope": scope.value,
+        "validity": validity.value,
+        "enable": true,
+        "establishment": establishments.value.length > 0 ? establishments.value[0] : "",
+        "expiredAt": moment(dateTo.value).toISOString(),
+        "createdAt": moment().toISOString(),
+    };
+
+    try {
+        if (category.value.length > 0) {
+            showSpinner.value = true;
+
+            if (type.value === 'add') {
+                // Logique pour ajouter un nouvel avantage
+                const response = await new Promise((resolve, reject) => {
+                    services.createRecord('advantages', advantageData, (response) => {
+                        resolve(response);
+                    });
+                });
+
+                if (response.status === 201) {
+                    // loadData(response.data, advantageData);
+                    ElMessage({
+                        message: `Advantage added successfully.`,
+                        type: 'success',
+                    });
+                    category.value = '';
+                    code.value = '';
+                    advantageName.value = '';
+                    amount.value = '';
+                    metric.value = '';
+                    scope.value = '';
+                    validity.value = '';
+                    establishments.value = [];
+                    dateTo.value = '';
+                    showSpinner.value = false;
+
+                }
+            } else if (type.value === 'edit' && advantage_to_update.value !== null) {
+                // Logique pour mettre à jour un avantage existant
+                const advantageId = advantage_to_update.value.id;
+                const response = await new Promise((resolve, reject) => {
+                    services.putRecord('advantages', advantageId, advantageData, (response) => {
+                        resolve(response);
+                    });
+                });
+                
+                if (response.status === 200) {
+                    ElMessage({
+                        message: `Advantage updated successfully.`,
+                        type: 'success',
+                    });
+                    updateData(response.data);
+                    console.log(response.data);
+                    activeAdvantageTab.value = 'advantage_list';
+                    showSpinner.value = false;
+                }
+            }
+        } else {
+            ElMessage.error(`Please, provide all needed information to add/update an advantage`);
+            showSpinner.value = false;
+        }
+    } catch (error) {
+        console.error('Error during form submission:', error);
+    }
+};
+const fetchAdvantageList = async () => {
+    try {
+        const response = await new Promise((resolve, reject) => {
+            services.get_Record(`advantage/list`, (response) => {
+                resolve(response);
+            });
+        });
+        console.log('Updated Advantage List:', response.data);
+        if (response.status === 200) {
+            allAdvantages.value = response.data;
+            console.log('Processed Advantage Data:', allAdvantages.value);
+        } else {
+            console.error('Error fetching advantages:', response);
+        }
+    } catch (error) {
+        console.error('Error fetching advantages:', error);
+    }
+};
+
+//     let event = {
+//         "name": eventName.value,
+//         "category": category.value,
+//         "datefrom": moment(dateFrom.value).format('YYYY-MM-DD'),
+//         "dateto":  moment(dateTo.value).format('YYYY-MM-DD'),
+//         "establishment": establishments.value
+//     }
+
+//     try {
+
+//         if(dateFrom.value != null && dateTo.value != null && category.value != '' && establishments.value.length >0 && eventName.value != ''){
+//            showSpinner.value = true;
+//             if(type.value == 'add'){
+//                 const response = await new Promise((resolve, reject) => {
+//                   services.createRecord('events', event, (response) => {
+//                     resolve(response);
+//                   });
+//                 });
+
+//                 if (response.status === 201) {
+//                   loadData(response.data, event);
+//                   ElMessage({
+//                     message: `Event added successfully.`,
+//                     type: 'success',
+//                   });
+//                   dateFrom.value = '';
+//                   dateTo.value = '';
+//                   category.value = '';
+//                   establishments.value = [];
+//                   eventName.value = '';
+//                   showSpinner.value = false;
+//                 }
+//             }else{
+//                 const response = await new Promise((resolve, reject) => {
+//                   services.putRecord('events', event_to_update.value['id'], event, (response) => {
+//                     resolve(response);
+//                   });
+//                 });
+
+//                 if (response.status == 200) {
+//                   updateData(response.data);
+//                   ElMessage({
+//                     message: `Event updated successfully.`,
+//                     type: 'success',
+//                   });
+//                   activeEventTab.value= 'event_list';
+
+//                   dateFrom.value = '';
+//                   dateTo.value = '';
+//                   category.value = '';
+//                   establishments.value = [];
+//                   eventName.value = '';
+//                   showSpinner.value = false;
+//                   type.value= 'add'
+//                 }
+//             }
+//         }else{
+//             ElMessage.error(`Please, provide all needed information to ${type.value} an event`);
+//             showSpinner.value = false;
+//         }  
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+</script>
+<style scoped>
+form{
+    height: 800px !important;
+}
+
+form button{
+    width: 8rem !important;
+}
+.security__header {
+    display: flex;
+    justify-content: space-between;
+}
+
+.security__header h4 {
+    color: var(--color-bg2);
+    font-size: 19px;
+    font-weight: bold;
+}
+
+.security__header p {
+    font-size: 15px;
+    margin: 8px 0;
+}
+
+input, select{
+    border-radius: 4px !important;
+    background-color: white;
+}
+
+label {
+    font-weight: 500 !important;
+    font-size: 14px !important;
+    color: var(--color-bg2) !important;
+}
+
+label span{
+    color: red;
+}
+
+input{
+    caret-color: var(--light-color-bg2);
+}
+
+.demo-tabs{
+    /* min-height: inherit !important; */
+}
+
+.demo-tabs > .el-tabs__content {
+  padding: 32px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+</style>
---
+++ src/components/advantage/AdvantageItemComponent.vue
@@ -0,0 +1,315 @@
+<template>
+    <div class="staff__list">
+        <div class="staff__card" v-if="events.length>0" v-for="event in events">
+            <ul class="mb-5">
+                <li><h5 :style="{
+                    color: `${generateColor(event.name)}`,
+                    fontWeight: 600,
+                }">{{ event.name }}</h5></li>
+                <li class="event_category"><span class="label">Category: </span> <span>{{ event.category }}</span></li>
+                <li class="period"><i class="uil uil-calender"></i> <span>{{ moment(event.datefrom).format('DD MMMM YYYY') }}</span> <span v-if="event.dateto != null">{{ `to ${moment(event.dateto).format('DD MMMM YYYY')}` }}</span></li>
+            </ul>
+            <div class="list__actions">
+                    <button 
+                        class="btn chart" 
+                        @click="showEventChart(event)">
+                        <i class="uil uil-chart-pie-alt"></i> View Chart
+                    </button>
+            </div>
+        </div>
+    </div>
+    <div v-if="events.length==0">No Event</div>
+      <ModalComponent :showModal="showChart" @close="showChart=false">
+                    <template #content>
+                        <div class="modal__header">
+                            <div class="modal__title">
+                                <h3 class="font-semibold text-gray-900 dark:text-white">
+                                    <i class="uil uil-chart-pie-alt"></i> Graphic Chart
+                                </h3>
+                            </div>
+                            <div class="modal__close">
+                                <i class="uil uil-times-circle"  @click="showChart = false"></i>
+                            </div>
+                        </div>
+
+                       
+                        <div class="pie__chart">
+                            <div>
+                                <h3 class="mb-2">-90 days to event (<span class="rating">{{calculateAverageRating(eventRatingDataset(eventComparison, 'beforeData'))}}</span>)</h3>
+                                <Pie 
+                                    :data="eventRatingDataset(eventComparison, 'beforeData')" 
+                                    :options="options" 
+                                />
+                            </div>
+                            <div>
+                                <h3 class="mb-2">During event (<span class="rating">{{calculateAverageRating(eventRatingDataset(eventComparison, 'duringData'))}}</span>)</h3>
+                                <Pie 
+                                    :data="eventRatingDataset(eventComparison, 'duringData')" 
+                                    :options="options" 
+                                />
+                            </div>
+                            <div>
+                                <h3 class="mb-2">Event +90 days  (<span class="rating">{{calculateAverageRating(eventRatingDataset(eventComparison, 'afterData'))}}</span>)</h3>
+                                <Pie 
+                                    :data="eventRatingDataset(eventComparison, 'afterData')" 
+                                    :options="options" 
+                                />
+                            </div>
+                        </div>
+                        <BaseLegend class="legend" :LegendData="legendData" :alignment="'horizontal'">
+                        </BaseLegend>
+                    </template>
+    </ModalComponent>
+</template>
+<script setup>
+import {ref, inject, onBeforeMount, computed, defineAsyncComponent} from 'vue';
+import moment from 'moment';
+import { Chart as ChartJS, ArcElement, Tooltip } from 'chart.js'
+import { Pie } from 'vue-chartjs';
+import { useCompanyStore } from "@Stores/company.js";
+import services from '@Services/services.js';
+
+ChartJS.register(ArcElement, Tooltip)
+
+const companiesStore = useCompanyStore();
+const events = inject('events');
+const establishment = inject('establishment');
+const showModal = ref(false);
+const showChart = ref(false);
+const selectedEvent = ref({})
+const eventComparison = ref({});
+const legendData = ref([
+    {name: '1 star', color: '#FF0000'},
+    {name: '2 stars', color: '#FFA500'},
+    {name: '3 stars', color: '#FFFF00'},
+    {name: '4 stars', color: '#00FF00'},
+    {name: '5 stars', color: '#008000'},
+])
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+   plugins: {
+        legend: {
+            display: false,
+        }
+  },
+};
+
+const ModalComponent = defineAsyncComponent(()=>
+    import('@Components/utils/ModalComponent.vue')
+)
+
+const eventRatingDataset = (periods, type)=> {
+      return {
+        labels: periods.labels,
+        datasets: [
+          {
+            backgroundColor: [
+            '#FF0000',
+            '#FFA500',
+            '#FFFF00',
+            '#00FF00',
+            '#008000',
+            ],
+            data: periods[`${type}`], //beforeData, duringData, afterData
+          },
+        ],
+      };
+};
+
+const calculateAverageRating = (data) =>  {
+  const starRatings = [1, 2, 3, 4, 5];
+  const ratingsData = data.datasets[0].data;
+
+  // Calcul de la somme pondérée des évaluations
+  let weightedSum = 0;
+  for (let i = 0; i < starRatings.length; i++) {
+    weightedSum += starRatings[i] * ratingsData[i];
+  }
+
+  // Calcul de la moyenne
+  const totalRatings = ratingsData.reduce((total, count) => total + count, 0);
+  const averageRating = weightedSum / totalRatings;
+  if(isNaN(averageRating.toFixed(1))) return 0;
+  return averageRating.toFixed(1);
+};
+
+const loadDataFromServer = async(id)=>{
+    const response = await new Promise((resolve, reject) => {
+        services.get_Record(`/event/periods?id=${id}`, (response) => {
+            resolve(response)
+        });
+    });
+
+    if (response.status == 200) {
+       eventComparison.value = response.data['data']
+       showChart.value = true;
+    }
+}
+
+const hashString = (inputString) => {
+      let hash = 0;
+      for (let i = 0; i < inputString.length; i++) {
+        hash = (hash << 5) - hash + inputString.charCodeAt(i);
+      }
+      return hash;
+}
+
+const generateColor = (text) =>{
+      const inputString = text;
+      const hash = hashString(inputString);
+
+      const red = (hash & 0xFF0000) >> 16;
+      const green = (hash & 0x00FF00) >> 8;
+      const blue = hash & 0x0000FF;
+
+      return `rgb(${red}, ${green}, ${blue})`;
+};
+
+const showEventChart = (event)=>{
+    selectedEvent.value = event;
+    showChart.value = true;
+    loadDataFromServer(event.id)
+};
+
+</script>
+<style scoped>
+.staff__list{
+    display: flex;
+    gap: 1rem;
+    flex-wrap: wrap;
+}
+
+.modal__header{
+    display: flex;
+    justify-content: space-between;
+}
+
+.modal__header div{
+    align-self: center;
+}
+
+.modal__close i{
+   float: right;
+   font-size: 25px;
+   color: red;
+   cursor: pointer;
+   transition: var(--transition);
+}
+
+.modal__close i:hover{
+    transform: rotate(360deg);
+}
+
+/*.staff__card{
+    border: 1px solid var(--light-color-bg2);
+    padding: 5px;
+    flex-basis: 400px;
+    flex-grow: 1;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    border-radius: 5px;
+    height: 250px;
+}*/
+
+.staff__card{
+    border: 1px solid var(--light-color-bg2);
+    padding: 15px;
+    flex-basis: 500px;
+    flex-grow: 1;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    border-radius: 5px;
+    display: flex;
+    gap:1rem;
+    flex-direction: column;
+    margin-bottom: 10px;
+}
+
+.staff__card h5{
+    color: var(--color-primary);
+    font-weight: 500;
+    font-size: 16px;
+}
+
+.uil-mars{
+    color: blue;
+}
+
+.uil-venus{
+    color: pink;
+}
+
+span.rating{
+    color: var(--color-danger);
+    font-weight: 600;
+}
+
+span{
+    font-size: 14px;
+    color: var(--color-bg2);
+}
+span.label{
+    color: var(--color-bg1);
+    font-size: 14px;
+}
+
+.pie__chart{
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+    height: 150px;
+}
+
+.pie__chart div{
+    width: 30% !important;
+    height: 100px !important;
+}
+
+.pie__chart h3{
+    text-align: center;
+    font-weight: 500;
+    color: var(--color-bg1);
+    font-size: 14px;
+}
+
+.period span{
+    font-weight: 500;
+}
+
+.period i{
+   color: var(--color-danger)
+}
+
+.event_category{
+     font-weight: 500;
+}
+
+.list__actions{
+    display: flex;
+    justify-content: flex-end;
+}
+
+.list__actions button{
+    border: 1px solid var(--light-color-bg1);
+    transition: var(--transition);
+    border-radius: 5px;
+    font-size: 13px;
+    font-weight: 500;
+    padding: 2px 6px;
+}
+
+.list__actions button.chart{
+    color: var(--color-primary);
+    border-color: var(--color-primary);
+}
+
+.list__actions button.chart:hover{
+    color: white;
+    background-color: var(--color-primary);
+}
+
+.list__actions button:hover{
+    transform: scale(0.95);
+}
+
+</style>
