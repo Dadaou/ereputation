22de4090b03c2fe8f80e8a3c306ed47cf7e7dc8e
Johary <joharyandrianjafimanohisolo@gmail.com>
2024-01-16 17:12:56 +0300

    range of date to date range

--- src/views/StaffRankingPageView.vue
+++ src/views/StaffRankingPageView.vue
@@ -199,7 +199,7 @@
                     </div>
 
                     <div class="date__filter">
-                        <div class="text-sm title">Select a range of date</div>
+                        <div class="text-sm title">Select a date range</div>
                         <el-date-picker v-model="start_date" type="date" placeholder="Select the start date"
                             :size="'large'" />
                         <el-date-picker class="mt-2" v-model="end_date" type="date" placeholder="Select the end date"
