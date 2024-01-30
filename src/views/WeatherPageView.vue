8442bb086797ed2fd47cea8eadf10c74e0aebb1d
laurenz19 <laurenziosambany@gmail.com>
2024-01-30 16:28:58 +0300

    correction de l'affichage weather

--- src/views/WeatherPageView.vue
+++ src/views/WeatherPageView.vue
@@ -46,12 +46,19 @@
                                 'fontWeight': 'bold',
                             }">
                               <span v-html="conditionData.icon"></span> {{ conditionData.condition }} 
+                              
                             </td>
-                            <td class="px-6 py-4" :style="{
+                            <td class="px-6 py-4 condition" :style="{
                                 'color': conditionData.color,
                                 'fontWeight': 'bold',
                             }">
-                                {{ conditionData.note != 0 ? conditionData.note : '' }} 
+                                <span> {{ conditionData.note != 0 ? conditionData.note : '' }}</span>
+                                <span class="bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded me-2 dark:bg-gray-700 dark:text-gray-400">
+                                <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z"/>
+                                </svg>
+                                {{conditionData.days}} {{conditionData.days>1?'days':'day'}}
+                                </span>
                             </td>
                         </tr>
                     </tbody>
@@ -408,7 +415,7 @@ const getIcon = (weatherConditions)=>{
 
 const formattedWeatherRating = computed(() => {
     const unit = nbDays.value>1?'days':'day'
-    const order = ['Clear', 'Partially cloudy', 'Overcast', 'Rain, Partially cloudy', 'Rain, Overcast', 'Rain', 'Snow Rain Overcast']
+    const order = ['Clear', 'Partially cloudy', 'Overcast', 'Rain, Partially cloudy', 'Rain Partially cloudy', 'Rain, Overcast', 'Rain Overcast', 'Rain', 'Snow Rain Overcast']
     let conditions = []
     if (weatherRating.value == null) return [];
     else {
@@ -416,8 +423,9 @@ const formattedWeatherRating = computed(() => {
         let data = weatherRating.value.conditions.map(condition => ({
             condition,
             icon: getIcon(condition),
-            note: `${weatherRating.value[condition].note} (${weatherRating.value[condition].days} ${unit})`,
-            color: weatherRating.value[condition].color
+            note: weatherRating.value[condition].note,
+            color: weatherRating.value[condition].color,
+            days: weatherRating.value[condition].days
         }));
 
         order.forEach(condition =>{
@@ -426,7 +434,12 @@ const formattedWeatherRating = computed(() => {
             }
         })
 
-        conditions.unshift({ condition: 'Average rating', note: `${weatherRating.value['rating']} (${nbDays.value} ${unit})`, color: 'green' })
+        conditions.unshift({ 
+            condition: 'Average rating', 
+            note: weatherRating.value['rating'], 
+            color: 'green',
+            days: nbDays.value
+        })
 
         return conditions;
     }
@@ -507,3 +520,10 @@ onBeforeMount(async () => {
     })
 });
 </script>
+
+<style scoped>
+.condition{
+    display: flex;
+    justify-content: space-between;
+}
+</style>
