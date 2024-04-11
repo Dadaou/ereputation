e83fe1bf6bb87883e667fdbad9e6ff46060e4f0d
laurenz19 <laurenziosambany@gmail.com>
2024-04-11 19:31:14 +0300

    mise en place du document qr code

--- src/views/QRCodeDocumentPreview.vue
+++ src/views/QRCodeDocumentPreview.vue
@@ -1,49 +1,75 @@
 <template>
-<div class="container preview">
-	<!-- <vue3-simple-html2pdf
-    ref="vue3SimpleHtml2pdf"
-    :options="pdfOptions"
-    :filename="exportFilename"
-  > -->
-    <div class="main-content">
-      Dear Customer,
-
-			At Nexties, we value your feedback as it helps us enhance your experience. We invite you to share your thoughts by scanning the QR code below.
-
-			As a token of our appreciation, you'll unlock exclusive benefits and surprises upon completing the feedback. Your input is crucial in shaping the future of our services.
-
-			Thank you for being a valued part of our community. Together, let's make Nexties even better!
-    </div>
-  <!-- </vue3-simple-html2pdf>
-
-  <button @click="download">Download pdf</button> -->
-</div>
+  <div id="preview" style="font-family: Arial, sans-serif;">
+    <p>Dear Customer,</p>
+    <p>At Nexties, we value your feedback as it helps us enhance your experience. We invite you to share your thoughts by scanning the QR code below.</p>
+    <p>As a token of our appreciation, you'll unlock exclusive benefits and surprises upon completing the feedback. Your input is crucial in shaping the future of our services.</p>
+    <qrcode-vue id="qrcode"
+    value="https://example.com/feedback"
+    :size="150" level="L" render-as="svg" />
+    <p>Thank you for being a valued part of our community. Together, let's make Nexties even better!</p>
+  </div>
+  <div id="qrCodeContainer"></div>
+  <button @click="generatePdf">Télécharger en PDF</button>
 </template>
+
 <script setup>
-import { onBeforeMount, ref } from 'vue';
+import { onBeforeMount } from 'vue';
 import { useAppStore } from "@Stores/app.js";
 import { useRoute } from "vue-router";
+import jsPDF from 'jspdf';
+import QrcodeVue from 'qrcode.vue';
+import QRCode from 'qrcode';
 
 const appStore = useAppStore();
 const route = useRoute();
-const vue3SimpleHtml2pdf = ref(null)
-const pdfOptions = {
-    margin: 15,
-      image: {
-        type: "jpeg",
-        quality: 1,
-    },
-    html2canvas: { scale: 3 },
-    jsPDF: {
-      unit: "mm",
-      format: "a4",
-      orientation: "p",
-    },
-}
-// const exportFilename =  "my-custom-file.pdf",
-const download = ()=> {
-   // vue3SimpleHtml2pdf.download();
-}
+const doc = new jsPDF({
+    orientation: 'portrait',
+    format: 'a5',
+});
+
+const generatePdf = () => {
+  addContentToPdf();
+  doc.save('preview.pdf');
+};
+
+const addContentToPdf = () => {
+ const content = `
+    Dear Customer,\n\n
+    At Nexties, we value your feedback as it helps us enhance your experience. We invite you to share your thoughts by scanning the QR code below.\n\n
+    As a token of our appreciation, you'll unlock exclusive benefits and surprises upon completing the feedback. Your input is crucial in shaping the future of our services.\n\n
+ `;
+
+ const text2 = `Thank you for being a valued part of our community. Together, let's make Nexties even better!\n`
+ doc.setFontSize(10);
+ const maxWidth = 140; 
+ const lines = doc.splitTextToSize(content, maxWidth);
+
+ let y = 25;
+ for (let i = 0; i < lines.length; i++) {
+    doc.text(lines[i], 6, y); 
+    y += 4; 
+ }
+ generateQRCode();
+ const lines2 = doc.splitTextToSize(text2, maxWidth)
+ y +=35
+ for (let i = 0; i < lines2.length; i++) {
+    doc.text(lines2[i], 6, y); 
+    y += 3;
+ }
+};
+
+
+const generateQRCode = () => {
+  const qrCodeData = 'https://example.com/feedback'; // Example URL for feedback
+  QRCode.toCanvas(document.getElementById('qrCodeContainer'), qrCodeData, { width: 100, height: 100 }, (error, canvas) => {
+    if (!error) {
+      const imageData = canvas.toDataURL('image/png');
+      doc.addImage(imageData, 'PNG', 50, 60, 50, 50);
+    } else {
+      console.error('QR Code generation error:', error);
+    }
+  });
+};
 
 onBeforeMount(()=>{
   appStore.setCurrentPage({
@@ -64,12 +90,22 @@ onBeforeMount(()=>{
         isCurrent: true
      }
    ])
+   generateQRCode();
 });
 </script>
 <style scoped>
-.preview{
-	margin-top: 5rem; 
+#preview{
+	margin: auto;
+	margin-top: 3rem;
+  width: 50%;
 }
 
-	
+#qrcode{
+  margin: auto;
+}
+
+#preview p{
+  padding: 15px 0;
+}
 </style>
+
