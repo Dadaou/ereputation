d474fb8e1bff908073a78c2ec9bae76516f5bc0d
Daniel <maherilanjadaniel@gmail.com>
2024-05-14 17:16:00 +0300

    user initial couleur ok

--- src/components/layouts/NavbarComponent.vue
+++ src/components/layouts/NavbarComponent.vue
@@ -135,10 +135,13 @@ watch(y, () => {
   if (y.value > 0) {
     nav__ref.value.classList.add('nav__onScroll');
     isScrolling.value = true;
-    console.log(nav__ref)
+    document.getElementById("userinitial").classList.remove('initial')
+    document.getElementById("userinitial").classList.add('initial_scroll')
   } else if (y.value <= 1) {
     nav__ref.value.classList.remove('nav__onScroll');
     isScrolling.value = false;
+    document.getElementById("userinitial").classList.add('initial')
+    document.getElementById("userinitial").classList.remove('initial_scroll')
   }
 });
 
