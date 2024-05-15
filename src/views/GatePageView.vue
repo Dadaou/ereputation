a393a79b9337c0beee6f84728e6ddb27d43a156f
Johary <joharyandrianjafimanohisolo@gmail.com>
2024-05-15 11:18:32 +0300

    links offers

--- src/views/GatePageView.vue
+++ src/views/GatePageView.vue
@@ -16,6 +16,9 @@
                 <div v-if="category == 'follow'" class="list__container">
                     <GateLinkComponent v-for="(item, index) in followLinks" :item="item" type="Social" :key="index" />
                 </div>
+                <div v-if="category == 'offers'" class="list__container">
+                    <GateLinkComponent v-for="(item, index) in offerLinks" :item="item" type="Offer" :key="index" />
+                </div>
             </div>
         </div>
     </div>
@@ -129,6 +132,13 @@ const platformLinks = computed(() => {
 const followLinks = computed(() => {
     if (links.value) {
         return links.value['Social']
+    }
+    return []
+})
+
+const offerLinks = computed(() => {
+    if (links.value) {
+        return links.value['Offers']
     }
     return []
 })
