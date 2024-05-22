ecb9f1dea81a7f7a47481f344168f5d533533e98
laurenz19 <laurenziosambany@gmail.com>
2024-05-22 13:54:28 +0300

    Advantage contact

--- src/components/advantage/AdvantageListComponent.vue
+++ src/components/advantage/AdvantageListComponent.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="security__header border__bottom">
-    <!-- <div class="security__edit">
-      <h4><i class="uil uil-calender"></i> Advantage List</h4>
-    </div> -->
   </div>
   <div class="mt-5 erep_table table__container">
     <el-table :data="filterTableData" class="responsive-table" style="width: 100%">
