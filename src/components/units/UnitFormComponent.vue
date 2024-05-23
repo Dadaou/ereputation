3c4dce55419d69c914cb5d6478d2b4f8ea4e44f9
Johary <joharyandrianjafimanohisolo@gmail.com>
2024-05-23 11:09:29 +0300

    merge

--- src/components/units/UnitFormComponent.vue
+++ src/components/units/UnitFormComponent.vue
@@ -42,24 +42,14 @@
                     </el-select>
                 </div>
             </div>
-            <<<<<<< HEAD <div
-                class="flex items-center justify-between px-3 py-2 border-t border-b dark:border-gray-600">
+            <div class="flex items-center justify-between py-2 border-t border-b dark:border-gray-600">
                 <button type="submit"
                     class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
-                    <SpinnerComponent :show-spinner="showSpinner" :color="'gray'" /> <span v-if="showSpinner">Loading
-                        ...</span>
+                    <SpinnerComponent :show-spinner="showSpinner" :color="'gray'" />
+                    <span v-if="showSpinner">Loading ...</span>
                     <span v-show="!showSpinner"><i class="uil uil-save"></i> {{ type }} service</span>
                 </button>
-                =======
-                <div class="flex items-center justify-between py-2 border-t border-b dark:border-gray-600">
-                    <button type="submit"
-                        class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
-                        <SpinnerComponent :show-spinner="showSpinner" :color="'gray'" />
-                        <span v-if="showSpinner">Loading ...</span>
-                        <span v-show="!showSpinner"><i class="uil uil-save"></i> {{ type }} service</span>
-                    </button>
-                    >>>>>>> f0a040aca1a98ed79504856acb965b71caa4fa50
-                </div>
+            </div>
         </form>
     </div>
 </template>
