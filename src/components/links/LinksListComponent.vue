e411f91e54af210dc3698981930fd2938429e4a8
Avotiana999 <avotianarandrianatrehana8@gmail.com>
2024-06-05 16:16:29 +0300

    section

--- src/components/links/LinksListComponent.vue
+++ src/components/links/LinksListComponent.vue
@@ -57,7 +57,7 @@ const filterTableData = computed(() => {
   let filteredData = tableData.value;
   filteredData = filteredData.filter((data) => {
 
-    if(data.section == 'REVIEWS' || data.section == 'FOLLOW US' || data.section == ''){
+    if(data.section == 'REVIEWS' || data.section == 'FOLLOW US' || data.section == '' || data.section == null){
       return (
         !search.value ||
         (data.source && data.source.toLowerCase().includes(search.value.toLowerCase())) ||
