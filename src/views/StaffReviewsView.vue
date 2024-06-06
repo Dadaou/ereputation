2645d6d2164a7a990ede89275d3448d55ae54aed
Ravaka15 <ravakiniainatokyhary@gmail.com>
2024-06-06 16:05:26 +0800

    ajout bloc de pagination en bas

--- src/views/StaffReviewsView.vue
+++ src/views/StaffReviewsView.vue
@@ -31,6 +31,10 @@
         </div>
     </div>
     <div v-else>No Reviews</div>
+    <div class="reviews__pagination">
+        <CommentPagination v-if="_reviews.length > 0" :config="paginationConfig" @updatePage="updatePage"
+            :color="'#6c63ff'" :nb="_reviews.length" :data="visibleData"></CommentPagination>
+    </div>
 </template>
 <script setup>
 import {
