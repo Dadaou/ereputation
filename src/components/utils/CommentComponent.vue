37efcc976a9d303ecafae4b80475a86ae903e2f9
Avotiana999 <avotianarandrianatrehana8@gmail.com>
2024-06-12 18:59:36 +0300

    reviews

--- src/components/utils/CommentComponent.vue
+++ src/components/utils/CommentComponent.vue
@@ -24,10 +24,14 @@
                             <li v-else class="flex items-center"><i class="uil uil-calender"></i><span>
                                     {{ moment(review.created_at).format('D MMMM YYYY') }}
                                 </span></li>
-                            <li class="flex items-center"><i class="uil uil-map-pin-alt"></i>
-                                <span>{{ review.source }}</span>
-                                <span v-if="review.source === 'App (Private)' && review.unit_name">&nbsp;&nbsp;<em>{{ review.unit_name }}</em></span>
-                            </li>
+                                <li class="flex items-center">
+                                    <i class="uil uil-map-pin-alt"></i>
+                                    <span>{{ review.source }}</span>
+                                    <span v-if="review.source === 'App (Private)'">
+                                    &nbsp;&nbsp;<em v-if="review.unit_name">{{ review.unit_name }}</em>
+                                                <em v-else>{{ review.staff_firstname }} {{ review.staff_lastname }}</em>
+                                    </span>
+                                    </li>
                         </ul>
                     </div>
                 </div>
