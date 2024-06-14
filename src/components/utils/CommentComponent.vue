74bff5340e690697d097e93522f7167e8efc4000
Johary <joharyandrianjafimanohisolo@gmail.com>
2024-06-14 15:42:24 +0300

    review url

--- src/components/utils/CommentComponent.vue
+++ src/components/utils/CommentComponent.vue
@@ -15,7 +15,7 @@
                             <div class="font-medium dark:text-white">
                                 <p id="author__name">{{ review.author }}</p>
                             </div>
-                            <el-tooltip placement="top">
+                            <el-tooltip placement="top" v-if="review.url">
                                 <template #content> Reply </template>
                                 <a :href="review.url ? review.url : '#'" target="_blank">
                                     <!-- <Icon icon="basil:reply-outline" width="24px"
