30a98dc950a46488beeb573adb199e5b2414a581
Avotiana999 <avotianarandrianatrehana8@gmail.com>
2024-06-25 18:19:26 +0300

    lieen

--- src/components/categorization/CategorizationListComponent.vue
+++ src/components/categorization/CategorizationListComponent.vue
@@ -5,7 +5,17 @@
   <div class="mt-5 table__container">
     <el-table :data="filterTableData">
       <el-table-column label="Category" prop="category" style="width: 15%; min-width: 300px;" />
-      <el-table-column label="Establishment" prop="establishment_name" style="width: 20%; min-width: 300px;" />
+      <el-table-column label="Establishment" prop="establishment_name" style="width: 20%; min-width: 300px;" >
+           <template #default="scope">
+                  <div class="establishment-link">
+            <el-tooltip :content="`Click to enter ${scope.row.establishmentNSame}'s page`" placement="top">
+              <span class="establishment_name" @click="redirectToEstablishment(route.params.tag, scope.row.establishment_tag)">
+                {{scope.row.establishment_name }}</span>
+        
+            </el-tooltip>
+          </div>
+                </template>
+            </el-table-column>
       <el-table-column style="width: 15%; min-width: 200px;" align="right">
         <template #header>
           <el-input v-model="search" size="small" placeholder="Type to search" class="searchtab"/>
@@ -24,6 +34,7 @@
 </template>
 <script setup>
 import { computed, ref, inject } from 'vue';
+import { useAppStore } from "@Stores/app.js";
 import { ElMessage, ElTable, ElTableColumn, ElPopconfirm, ElButton, ElInput } from 'element-plus';
 import 'element-plus/es/components/message/style/css'
 import 'element-plus/es/components/table/style/css'
@@ -31,11 +42,19 @@ import 'element-plus/es/components/table-column/style/css'
 import 'element-plus/es/components/popconfirm/style/css'
 import 'element-plus/es/components/button/style/css'
 import 'element-plus/es/components/input/style/css'
+import 'element-plus/es/components/option/style/css'
+import 'element-plus/es/components/message/style/css'
+import 'element-plus/es/components/select/style/css'
+import 'element-plus/es/components/tooltip/style/css'
 import services from '@Services/services.js';
+import { useRoute, useRouter } from "vue-router";
 
+const appStore = useAppStore(); 
 const emit = defineEmits(['edit']);
 const categories = inject('categories');
 const search = ref('')
+const route = useRoute()
+const router = useRouter()
 
 const filterTableData = computed(() => {
   let filterdata = categories.value;
@@ -76,8 +95,18 @@ const handleDelete = async (index, category) => {
   }
 };
 
+const redirectToEstablishment = async(customer, establishment)=>{
+    router.push(`/customer/${customer}/establishment/${establishment}`)
+}
+
+
 </script>
 <style scoped>
+.establishment_name {
+  cursor: pointer;
+font-weight: 500;
+}
+
 button {
   border: none;
   cursor: pointer;
