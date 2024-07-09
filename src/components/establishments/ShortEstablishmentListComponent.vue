f00fb536aef72f47a00e4d0dec3f2aebfb374016
Avotiana999 <avotianarandrianatrehana8@gmail.com>
2024-07-09 15:45:04 +0300

    correc

--- src/components/establishments/ShortEstablishmentListComponent.vue
+++ src/components/establishments/ShortEstablishmentListComponent.vue
@@ -200,7 +200,7 @@ const getValueUrl = (url, urlTemplate) => {
 const getParametersEstablishments = async(customer)=>{
     
     try {
-        const userId = userStore.userId;
+        const userId = userStore.user.id;
         const response = await new Promise((resolve) => {
             services.get_Record(`/customer/establishments/parameters?tag=${customer}&user_id=${userId}`, (response) => {
                 resolve(response);
