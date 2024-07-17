5f9bc92b77f4d991dd017184059a7020e98346a0
AnitaRak <anitatantely@gmail.com>
2024-07-17 09:33:04 +0300

    collecte donné

--- src/views/GatePageView.vue
+++ src/views/GatePageView.vue
@@ -11,34 +11,34 @@
                 <div class="panel" v-if="item.active">
                     <div v-if="category == 'reviews'"
                         class="list__container grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-8 xl:grid-cols-10">
-                        <GateLinkComponent v-for="(element, index) in establishmentLink" :item="element"
-                            type="Establishment" :key="index" />
-                        <GateLinkComponent v-for="(element, index) in staffLinks" :item="element" type="staff"
-                            :key="index" />
-                        <GateLinkComponent v-for="(element, index) in unitLinks" :item="element" type="Unit"
-                            :key="index" />
-                        <GateLinkComponent v-for="(element, index) in platformLinks" :item="element" type="Platform"
-                            :key="index" />
+                        <GateLinkComponent @click="handleClick(element,category)" v-for="(element, index) in establishmentLink"
+                            :item="element" type="Establishment" :key="index" />
+                        <GateLinkComponent @click="handleClick(element,category)" v-for="(element, index) in staffLinks"
+                            :item="element" type="staff" :key="index" />
+                        <GateLinkComponent @click="handleClick(element,category)" v-for="(element, index) in unitLinks"
+                            :item="element" type="Unit" :key="index" />
+                        <GateLinkComponent @click="handleClick(element,category)" v-for="(element, index) in platformLinks"
+                            :item="element" type="Platform" :key="index" />
                     </div>
                     <div v-if="category == 'follow'"
                         class="list__container grid-cols-3 sm:grid-cols-5 md:grid-cols-7 lg:grid-cols-9 xl:grid-cols-12">
-                        <GateLinkComponent v-for="(element, index) in followLinks" :item="element" type="Social"
-                            :key="index" />
+                        <GateLinkComponent @click="handleClick(element,category)" v-for="(element, index) in followLinks"
+                            :item="element" type="Social" :key="index" />
                     </div>
                     <div v-if="category == 'offers'"
                         class="list__container grid-cols-3 sm:grid-cols-5 md:grid-cols-7 lg:grid-cols-9 xl:grid-cols-12">
-                        <GateLinkComponent v-for="(element, index) in offerLinks" :item="element" type="Offer"
-                            :key="index" />
+                        <GateLinkComponent @click="handleClick(element,category)" v-for="(element, index) in offerLinks"
+                            :item="element" type="Offer" :key="index" />
                     </div>
                     <div v-if="category == 'infos'"
                         class="list__container grid-cols-3 sm:grid-cols-5 md:grid-cols-7 lg:grid-cols-9 xl:grid-cols-12">
-                        <GateLinkComponent v-for="(element, index) in infoLinks" :item="element" type="Info"
-                            :key="index" />
+                        <GateLinkComponent @click="handleClick(element,category)" v-for="(element, index) in infoLinks"
+                            :item="element" type="Info" :key="index" />
                     </div>
                     <div v-if="category == 'menus'"
                         class="list__container grid-cols-3 sm:grid-cols-5 md:grid-cols-7 lg:grid-cols-9 xl:grid-cols-12">
-                        <GateLinkComponent v-for="(element, index) in menuLinks" :item="element" type="Menu"
-                            :key="index" />
+                        <GateLinkComponent @click="handleClick(element,category)" v-for="(element, index) in menuLinks"
+                            :item="element" type="Menu" :key="index" />
                     </div>
                 </div>
             </div>
@@ -86,6 +86,20 @@ const useCategories = computed(() => {
     }*/
     return categories.value
 })
+
+const handleClick = (element,category) => {
+    const visitorId = localStorage.getItem('visitId');
+    const click_label_option = element.label + " " + "logo";
+    const click_source = "gates";
+    const click_label = category;
+    if (element.document) {
+        window.open(element.document, '_blank');
+    } else if (element.href) {
+        window.open(element.href, '_blank');
+    } else {
+        console.log('No valid URL found in element');
+    }
+};
 
 const toggleMenu = (item) => {
     category.value = item.value
--- src/views/SuccessMessageFeedback.vue
+++ src/views/SuccessMessageFeedback.vue
@@ -12,7 +12,7 @@
                 <h2 v-if="links.length > 0">{{ $t("success_text") }}</h2>
                 <ul v-if="links.length > 0" class="logoSrc">
                     <li v-for="link in links" :key="link.id">
-                        <a :href="link.url" target="_blank">
+                        <div @click="handleClick($event,link)">
                             <el-tooltip :content="`${$t('success_text')} ${link.name}`" placement="top">
                                 <img v-if="link.name.toLowerCase().includes('booking')"
                                     src="@/assets/images/logo/Booking.svg" alt="Booking" width="24" height="24">
@@ -38,7 +38,7 @@
                                     alt="Yelp" width="24" height="24">
 
                             </el-tooltip>
-                        </a>
+                        </div>
                     </li>
                 </ul>
                 <h2 v-if="socials.length > 0">{{ $t("success_text2") }}</h2>
@@ -95,6 +95,29 @@ const route = useRoute();
 const companyStore = useCompanyStore();
 const userStore = useUserStore();
 const appStore = useAppStore();
+
+const handleClick = (event,element) => {
+    const visitorId = localStorage.getItem('visitId');
+    const click_source = "feedback";
+    const click_label = ref(null);
+    console.log("click_label", click_label)
+
+    const imgElement = event.target;
+    if (imgElement.tagName === 'IMG') {
+        click_label.value = imgElement.alt;
+        console.log("Alt text of the image:", click_label.value);
+    } else {
+        console.log('No image found in element');
+    }
+
+
+    if (element.url) {
+        window.open(element.url, '_blank');
+    }
+    else {
+        console.log('No valid URL found in element');
+    }
+};
 
 onBeforeMount(async () => {
     appStore.setCurrentPage({
