b85606db31b76704a867d75a242cee997fe52088
AnitaRak <anitatantely@gmail.com>
2024-07-18 18:26:36 +0300

    page statistique avec donnée statique

--- src/components/utils/UserMenuDropdownComponent.vue
+++ src/components/utils/UserMenuDropdownComponent.vue
@@ -1,62 +1,67 @@
 <template>
 	<div class="flex items-center md:order-2" ref="menuContainer">
-	        <button type="button" @click="toggleDropdown" class="flex mr-3" id="user-menu-button" aria-expanded="false"
-	          data-dropdown-placement="bottom">
-	          <span class="sr-only">Open user menu</span>
-	          <div class="relative w-8 h-8 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500">
-	            <span class="font-medium initial" id="userinitial">{{ user.initial }}</span>
-	          </div>
-	        </button>
-	        <!-- Dropdown menu -->
-	        <transition 
-	        name="fade"
-	        enter-active-class="animate__animated animate__fadeInRight"
-  			leave-active-class="animate__animated animate__fadeOutRight" 
-	        >
-			  <div v-if="showDropdown" id="user-dropdown">
-		          <div class="px-4 py-3">
-		            <span class="block text-sm text-gray-900 dark:text-white">{{ user.name }}</span>
-		            <span class="block text-sm text-gray-500 truncate dark:text-gray-400">{{ user.email }}</span>
-		          </div>
-		          <ul class="py-2" aria-labelledby="user-menu-button">
-		            <li @click="closeDropdown" v-if="customer">
-		              <RouterLink :to="`/customer/${customer.tag}/account/personal_details`"
-		                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-		                <i class="uil uil-user-square"></i>
-		                Profile
-		              </RouterLink>
-		            </li>
+		<button type="button" @click="toggleDropdown" class="flex mr-3" id="user-menu-button" aria-expanded="false"
+			data-dropdown-placement="bottom">
+			<span class="sr-only">Open user menu</span>
+			<div class="relative w-8 h-8 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500">
+				<span class="font-medium initial" id="userinitial">{{ user.initial }}</span>
+			</div>
+		</button>
+		<!-- Dropdown menu -->
+		<transition name="fade" enter-active-class="animate__animated animate__fadeInRight"
+			leave-active-class="animate__animated animate__fadeOutRight">
+			<div v-if="showDropdown" id="user-dropdown">
+				<div class="px-4 py-3">
+					<span class="block text-sm text-gray-900 dark:text-white">{{ user.name }}</span>
+					<span class="block text-sm text-gray-500 truncate dark:text-gray-400">{{ user.email }}</span>
+				</div>
+				<ul class="py-2" aria-labelledby="user-menu-button">
 					<li @click="closeDropdown" v-if="customer">
-		              <RouterLink :to="`/customer/${customer.tag}/account/my_qrcodes`"
-		                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-		                <i class="uil-qrcode-scan"></i>
-		                My QrCOde
-		              </RouterLink>
-		            </li>
-		            <li @click="closeDropdown" v-if="customer">
-		              <RouterLink :to="`/customer/${customer.tag}/account/contact`"
-		                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-		                <i class="uil uil-envelope"></i>
-		                Contacts
-		              </RouterLink>
-		            </li>
-		            <li @click="closeDropdown" v-if="customer">
-		              <RouterLink :to="`/customer/${customer.tag}/account/parameters`"
-		                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-		                <i class="uil uil-setting"></i>
-		                Parameters
-		              </RouterLink>
-		            </li>
-		            <li @click="closeDropdown">
-		              <RouterLink to="/" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" @click="$emit('signOut')">
-		                <i class="uil uil-signout"></i>
-		                Sign out
-		              </RouterLink>
-		            </li>
-		          </ul>
-		        </div>
-			</transition>
-    </div>
+						<RouterLink :to="`/customer/${customer.tag}/account/personal_details`"
+							class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+							<i class="uil uil-user-square"></i>
+							Profile
+						</RouterLink>
+					</li>
+					<li @click="closeDropdown" v-if="customer">
+						<RouterLink :to="`/customer/${customer.tag}/account/my_qrcodes`"
+							class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+							<i class="uil-qrcode-scan"></i>
+							My QrCOde
+						</RouterLink>
+					</li>
+					<li @click="closeDropdown" v-if="customer">
+						<RouterLink :to="`/customer/${customer.tag}/account/statistique`"
+							class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+							<i class="uil-analytics"></i>
+							Statistique
+						</RouterLink>
+					</li>
+					<li @click="closeDropdown" v-if="customer">
+						<RouterLink :to="`/customer/${customer.tag}/account/contact`"
+							class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+							<i class="uil uil-envelope"></i>
+							Contacts
+						</RouterLink>
+					</li>
+					<li @click="closeDropdown" v-if="customer">
+						<RouterLink :to="`/customer/${customer.tag}/account/parameters`"
+							class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+							<i class="uil uil-setting"></i>
+							Parameters
+						</RouterLink>
+					</li>
+					<li @click="closeDropdown">
+						<RouterLink to="/" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+							@click="$emit('signOut')">
+							<i class="uil uil-signout"></i>
+							Sign out
+						</RouterLink>
+					</li>
+				</ul>
+			</div>
+		</transition>
+	</div>
 </template>
 
 <script setup>
---
+++ src/views/StatistiquePageView.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <h1>Statistique</h1>
+        <div class="dashboard">
+            <div class="statistique">
+                <ChartComponent />
+            </div>
+            <div class="statistique">
+                <Chart3Component />
+            </div>
+        </div>
+        <br>
+        <div class="dashboard">
+            <div class="statistique bas">
+                <Chart2Component />
+            </div>
+            <div class="statistique">
+                <Chart4Component />
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+import {defineAsyncComponent} from 'vue';
+
+
+const ChartComponent = defineAsyncComponent(() =>
+    import("@Components/ChartStatistique/ChartComponent.vue")
+)
+
+const Chart2Component = defineAsyncComponent(() =>
+    import("@Components/ChartStatistique/Chart2Component.vue")
+)
+
+const Chart3Component = defineAsyncComponent(() =>
+    import("@Components/ChartStatistique/Chart3Component.vue")
+)
+
+const Chart4Component = defineAsyncComponent(() =>
+    import("@Components/ChartStatistique/Chart4Component.vue")
+)
+
+
+
+</script>
+<style>
+.dashboard {
+    display: flex;
+    justify-content: space-around;
+}
+.bas {
+    margin-left : -50px ;
+}
+.statistique {
+    z-index: 2;
+}
+h1 {
+    font-size: 18px;
+    font-weight: 600;
+    margin-bottom: 15px;
+    color: var(--color-primary);
+}
+</style>
