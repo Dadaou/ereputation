5396b85a76059874269ea137abe1d6d719725667
Avotiana999 <avotianarandrianatrehana8@gmail.com>
2024-07-19 09:38:47 +0300

    debug

--- src/components/utils/EstablishmentsListComponent.vue
+++ src/components/utils/EstablishmentsListComponent.vue
@@ -39,7 +39,7 @@
                                     </div>
                                     <div class="reviews-count">
                                         <h2 v-if="showReviewsHeader">Reviews:</h2>
-                                        <div class="review-box" v-for="(count, stars) in sortedReviews(company.reviews_count)" :key="stars" @click="redirectToReviews(stars, company.competitor_tag)">
+                                        <div class="review-box" v-if="company.reviews_count" v-for="(count, stars) in sortedReviews(company.reviews_count)" :key="stars" @click="redirectToReviews(stars, company.competitor_tag)">
                                             <span class="score">{{ stars }}</span><i class="fa fa-star " aria-hidden="true"></i>: {{ count }} 
                                         </div>
                                     </div>
