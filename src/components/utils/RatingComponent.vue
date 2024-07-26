2214765ea657bf1a1338458e16cd9fb19eaf557d
Avotiana999 <avotianarandrianatrehana8@gmail.com>
2024-07-26 09:34:33 +0300

    logo

--- src/components/utils/RatingComponent.vue
+++ src/components/utils/RatingComponent.vue
@@ -187,49 +187,6 @@ const arrondirRatio = (ratio)=>{
         margin-left: -90px;
     }
   }
-  @media (max-width: 420px) {
-      .society__rating{
-        display: flex;
-        align-items: center;
-        margin-top: -5px;
-        margin-left: -90px;
-        padding-right: 50px;
-    }
-    .reviews{
-        font-size: 11px;
-        margin-left: -20px;
-        
-    }
-  }
  
-  @media (min-width: 383px) and (max-width: 390px) {
-      .society__rating{
-        display: flex;
-        align-items: center;
-        margin-top: -5px;
-        margin-left: -120px;
-        padding-right: 60px;
-    }
-    .reviews{
-        font-size: 11px;
-        margin-left: -20px;
-        
-    }
-  }
-
-  @media (max-width: 375px) {
-      .society__rating{
-        display: flex;
-        align-items: center;
-        margin-top: -5px;
-        margin-left: -120px;
-        padding-right: 60px;
-    }
-    .reviews{
-        font-size: 11px;
-        margin-left: -20px;
-        
-    }
-  }
 
 </style>
