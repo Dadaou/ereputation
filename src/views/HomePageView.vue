7636b1ab4779cecdee01646625f8f46188b32e8d
Ravaka15 <ravakiniainatokyhary@gmail.com>
2024-09-17 14:48:48 +0300

    modification icon classement categorization

--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -13,7 +13,7 @@
                     </RouterLink>
                     <RouterLink v-if="establishments.length > 1" class="search__icon"
                         :to="{ name: 'Categorization', params: { tag: tag } }">
-                        <Icon :icon="'carbon:category-new-each'" width="25"></Icon>
+                        <Icon :icon="'carbon:category'" width="25"></Icon>
                     </RouterLink>
                     <RouterLink class="search__icon"
                         :to="{ name: 'EstablishmentListByTrend', params: { tag: tag } }">
