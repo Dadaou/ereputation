3022fbe44f66e381215c7897028df5800544c8aa
Ravaka15 <ravakiniainatokyhary@gmail.com>
2024-09-20 05:51:49 +0300

    filtrage star et establishement

---
+++ src/views/CategorizationReview.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="admin__container">
+        <div class="filtre_content p-4">
+            <div class="left__side">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.filtre_content {
+    width: 100% !important;
+}
+
+.admin__container {
+    gap: 2rem;
+    margin-right: 5%;
+    margin-left: 5%;
+    margin-top: 120px !important;
+    overflow-x: hidden;
+}
+</style>
