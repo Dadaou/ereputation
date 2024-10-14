339a7ce2e186fe6b2e6d6930bd5a173d33d23815
Ravaka15 <ravakiniainatokyhary@gmail.com>
2024-10-14 15:04:59 +0300

    modif icon partnership amelioration gate

--- src/services/routes.js
+++ src/services/routes.js
@@ -75,7 +75,7 @@ export const homePageUrls = {
 
 export const homeLeadgenUrls = {
   LeadgenAdvantage : { label: 'Advantage', icon: 'uil:bill' },
-  LeadgenPartnership : { label: 'Partnership', icon: 'uil:users-alt' },
+  LeadgenPartnership : { label: 'Partnership', icon: 'lucide:handshake' },
   LeadgenDiscountCoupon : { label: 'Discount coupon', icon: 'uil:bill' },
   LeadgenContact : { label: 'Contact', icon: 'uil:envelope' },
   LeadgenMyQrcode : { label: 'My Qrcode', icon: 'uil:qrcode-scan' },
