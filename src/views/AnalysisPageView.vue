cb8483b21363ec314e2ee86826b0948c89ab27d9
Ravaka15 <ravakiniainatokyhary@gmail.com>
2024-11-07 17:56:37 +0300

    correction couleur

--- src/views/AnalysisPageView.vue
+++ src/views/AnalysisPageView.vue
@@ -528,7 +528,7 @@ const ratingChart = ref({
     datasets: []
 })
 // const colors = ['#6c63ff', '#f75842', '#aca8fd', '#424890', '#ff42e5', '#58f742', '#8eaca8', '#fda458', '#90fdac', '#444278', '#f7a142', '#de90fd', '#42d3ff', '#e558f7', '#a8ac42', '#90fdd4', '#784444', '#58f7bf', '#fdaa58', '#90fdff']
-const colors = ['#013B54', '#018BAD','#DA9A55','#4EA9CC', '#01DFFF', '#7FA8BB', '#B3DCE7',  '#01BDDE', '#573427', '#915138', '#E7CDB7', '#91CBDE','#A483BD', '#C7F5FA', '#01254F', '#014A93']
+const colors = ['#013B54', '#018BAD','#DA9A55','#4EA9CC', '#01DFFF', '#7FA8BB', '#0A8D87BF',  '#01BDDE', '#573427', '#915138', '#FCB08F', '#91CBDE','#A483BD', '#458EE9', '#01254F', '#014A93']
 
 const confidenceChart = ref({
     labels: [],
