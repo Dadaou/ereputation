586bcf4772b569dd49148f2d6467ad3e0475b712
Ravaka15 <ravakiniainatokyhary@gmail.com>
2024-11-19 18:39:23 +0300

    resize largeLogo

--- src/components/units/UnitFeedbackComponent.vue
+++ src/components/units/UnitFeedbackComponent.vue
@@ -18,7 +18,7 @@
                 </div>
                 <div class="photo">
                     <div v-if="establishment.url_source !== null" class="establishment__img">
-                        <img :src="establishment.url_source" alt="" id="logoimage"/>
+                        <img :src="establishment.url_source" alt="" id="logoimage" />
                     </div>
                     <div v-else role="status"
                         class="flex items-center justify-center max-w-sm bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
@@ -74,8 +74,8 @@
                             <span v-if="randomAdvantage">
                                 <i class="uil uil-info-circle"></i> {{ $t("feedback.indice1") }}
                             </span>
-                            <label for="email"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ $t("feedback.email") }} <!-- <span>*</span> --></label>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{
+                                $t("feedback.email") }} <!-- <span>*</span> --></label>
                             <input type="email" v-model="email" id="email"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
                         </div>
@@ -149,7 +149,7 @@
 
 <script setup>
 
-import { ref, onBeforeMount, defineAsyncComponent, computed, onMounted, inject,watch } from 'vue';
+import { ref, onBeforeMount, defineAsyncComponent, computed, onMounted, inject, watch } from 'vue';
 import RatingFeedbackComponent from '@Components/utils/RatingFeedbackComponent.vue';
 import { useRoute, useRouter } from "vue-router";
 import services from '@Services/services.js';
@@ -234,16 +234,16 @@ onBeforeMount(async () => {
 })
 const requiredinput = ref('');
 onMounted(() => {
-   
-   if (!route.query.preview) {
-     try {
-        if (window.FingerprintApp && window.FingerprintApp.default && typeof window.FingerprintApp.default.main === 'function') {
-            window.FingerprintApp.default.main();
+
+    if (!route.query.preview) {
+        try {
+            if (window.FingerprintApp && window.FingerprintApp.default && typeof window.FingerprintApp.default.main === 'function') {
+                window.FingerprintApp.default.main();
+            }
+        } catch (error) {
+            console.error("Une erreur s'est produite lors de l'exécution de Fingerprint :", error);
         }
-    } catch (error) {
-        console.error("Une erreur s'est produite lors de l'exécution de Fingerprint :", error);
     }
-   }
     requiredinput.value = t('feedback.requiredinputs')
 
     appStore.setCurrentPage({
@@ -253,7 +253,7 @@ onMounted(() => {
     });
 })
 
-watch(()=>{
+watch(() => {
     appStore.setCurrentPage({
         title1: t("feedback.title1"),
         title2: t("feedback.title2"),
@@ -323,13 +323,13 @@ const submit = async () => {
                         await services.createRecord('public/contacts', contactData, async (contactResponse) => {
                             if (contactResponse.status == 201) {
                                 if (visitorId) {
-                                    const visitor = { 
-                                        visitor: visitorId, 
+                                    const visitor = {
+                                        visitor: visitorId,
                                         contact: contactResponse.data.data['@id'],
                                     }
 
-                                    services.post_Record('public/visitors', visitor , (res) => {
-                                       console.log(res)
+                                    services.post_Record('public/visitors', visitor, (res) => {
+                                        console.log(res)
                                     }, true)
                                 }
 
@@ -342,7 +342,7 @@ const submit = async () => {
                                         email: email.value,
                                         language: (lg.toLowerCase() == 'sp') ? 'es' : lg.toLowerCase(),
                                         app_url: app_url.value,
-                                        template: "workflow_"+((lg.toLowerCase() == 'sp') ? 'es' : lg.toLowerCase())
+                                        template: "workflow_" + ((lg.toLowerCase() == 'sp') ? 'es' : lg.toLowerCase())
                                     }
                                     console.log(coupons)
                                     await services.createRecord('public/workflow', coupons, () => {
@@ -374,81 +374,85 @@ const submit = async () => {
 /**
  * obtenir width image from url
  */
- const getMeta = (url, cb) => {
-  const img = new Image();
-  img.onload = () => cb(null, img);
-  img.onerror = (err) => cb(err);
-  img.src = url;
+const getMeta = (url, cb) => {
+    const img = new Image();
+    img.onload = () => cb(null, img);
+    img.onerror = (err) => cb(err);
+    img.src = url;
 };
 
 /** Fonction widthimage pour savoir le width 
  * @param event 
 */
-const  widthimage = (event) => {
-    
+const widthimage = (event) => {
+
     document.getElementsByClassName("establishment__img").innerText = "Loading image...";
-    return getMeta(event,(err, img) =>{
+    return getMeta(event, (err, img) => {
         const heightresize = 150; //hauteur div pour l'image
         var aspectRatio = img.naturalWidth / img.naturalHeight;
         var newWidth = 0;
-        if(aspectRatio == 1){
+        if (aspectRatio == 1) {
             // ici carre
-            newWidth =heightresize;
-        }else{
-            newWidth =heightresize * aspectRatio;
-        }   
-        let classy =   (newWidth>210)? "largeClass" : "smallClass";
+            newWidth = heightresize;
+        } else {
+            newWidth = heightresize * aspectRatio;
+        }
+        let classy = (newWidth > 210) ? "largeClass" : "smallClass";
         // pour le desktop
         var elem = document.getElementById("logoimage");
         elem.classList.add("fade-in");
         setTimeout(() => {
             elem.classList.add('show');
-            
-            }, 10);
+
+        }, 10);
         elem.classList.add(classy);
         elem.src = event;
         //new Promise(resolve=>{elem.onload = resolve})
-       
+
         //pour le mobile
         var elemmob = document.getElementById("logoimagemobile");
-        if(elemmob !== null){
+        if (elemmob !== null) {
             elemmob.classList.add(classy);
-            
-            elemmob.src=event;
+
+            elemmob.src = event;
             // Ajouter la classe 'show' pour déclencher l'animation de fondu en entrée
             setTimeout(() => {
-                
+
                 elemmob.classList.add('show');
                 elemmob.style.display = "block";
             }, 10);
         }
-        
+
         return "OK";
     });
-   
+
 }
 
 </script>
 
 <style scoped>
-.smallClass{
-    width: auto! important;
-    height: 100%! important;
-    object-fit:contain! important;
+.smallClass {
+    width: auto ! important;
+    height: 100% ! important;
+    object-fit: contain ! important;
 }
-.establishment__img{
-    display:flex;
-    justify-content:center;
+
+.establishment__img {
+    display: flex;
+    justify-content: center;
 }
-.largeClass{
-    width: 100%! important;
-    height: auto! important;
-    object-fit:cover! important;
+
+.largeClass {
+    width: 100% ! important;
+    height: auto ! important;
+    object-fit: contain ! important;
 }
+
 .fade-in {
     opacity: 0;
     transition: opacity 1s ease-in;
 }
+
 .fade-in.show {
     opacity: 1;
 }
@@ -694,6 +698,12 @@ span.label {
 @media screen and (max-width:750px) {
     .feedback__form {
         width: 90%;
+    }
+}
+
+@media screen and (max-width:500px) {
+    .largeClass {
+        object-fit: fill ! important;
     }
 }
 </style>
