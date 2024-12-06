bb7f625e5301969aa0e94745030dc15fcd865306
Ravaka15 <ravakiniainatokyhary@gmail.com>
2024-12-06 14:39:23 +0300

    modif photo partnership

--- src/components/partnership/PartnershipListComponent.vue
+++ src/components/partnership/PartnershipListComponent.vue
@@ -9,7 +9,7 @@
     <el-table :data="filterTableDataSent" class="responsive-table">
       <el-table-column width="100">
         <template #default="scope">
-          <img class="establishment_img" :src="scope.row.establishment_logo" alt="Establishment Logo" />
+          <img class="establishment_img" :src="scope.row.partnership_logo" alt="Establishment Logo" />
         </template>
       </el-table-column>
       <el-table-column label="Advantage" prop="advantage_name" style="width: 15%; min-width: 200px;" />
