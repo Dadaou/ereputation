7d0a5978f52447e4f70d4780d6caeff1c19b9e90
Ravaka15 <ravakiniainatokyhary@gmail.com>
2024-12-09 16:06:31 +0300

    fix padding button request

--- src/components/partnership/PartnershipFormComponent.vue
+++ src/components/partnership/PartnershipFormComponent.vue
@@ -52,7 +52,7 @@
                 </div>
             </div>
             <AdvantagePartnershipList :items="allAdvantageList" class="mt-5" />
-            <div class="flex items-center justify-between px-3 py-2 border-t border-b dark:border-gray-600">
+            <div class="flex items-center justify-between py-2 border-t border-b dark:border-gray-600">
                 <button type="submit"
                     class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
                     <SpinnerComponent :show-spinner="showSpinner" :color="'gray'" /> <span v-if="showSpinner">Loading
