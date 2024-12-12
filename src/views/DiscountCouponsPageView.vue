6f92dcd461a1c1bcfc4bf05ff4a334ca7ed855e3
Ravaka15 <ravakiniainatokyhary@gmail.com>
2024-12-12 15:17:12 +0300

    remplacement case en sablier et modif en check vert

--- src/views/DiscountCouponsPageView.vue
+++ src/views/DiscountCouponsPageView.vue
@@ -49,9 +49,10 @@
       <el-table-column fixed="right" align="center" label="Confirm" :width="isMobile ? 70 : 200">
         <template #default="scope">
           <span v-if="scope.row.confirm && scope.row.other_customer == null" @click="handleCancel(scope.row.id)"
-            class="has-hover"><i class="uil uil-check-square" style="color: #777; font-size: 15px;"></i></span>
+            class="has-hover"><i class="uil uil-check mr-1"
+              style="color:var(--color-success); font-size: 16px;"></i></span>
           <span v-if="!scope.row.confirm && scope.row.other_customer == null" @click="handleConfirm(scope.row.id)"
-            class="has-hover"><i class="uil uil-square" style="color: #777; font-size: 15px;"></i></span>
+            class="has-hover"><i class="uil uil-dna" style="color: #777; font-size: 15px;"></i></span>
         </template>
       </el-table-column>
     </el-table>
