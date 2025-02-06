4caacbb9fa2128e1a9d46d40ff3f0ec6fe871479
Onja <ndriana0@gmail.com>
2025-02-06 16:07:50 +0300

    min 0

--- src/components/establishments/EstablishmentFormComponent.vue
+++ src/components/establishments/EstablishmentFormComponent.vue
@@ -103,7 +103,7 @@
                     <label for="positioning"
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Order
                     </label>
-                    <input type="number" id="positioning" name="positioning" v-model="data.positioning"
+                    <input type="number" id="positioning" name="positioning" v-model="data.positioning" min="0"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm w-full p-2">
                 </div>
                 <div>
