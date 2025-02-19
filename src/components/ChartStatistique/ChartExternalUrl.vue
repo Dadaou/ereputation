6d531a4e1aa069bd90974902cf67c2218f296c5c
Mahaiavy <mahaiavym@gmail.com>
2025-02-19 14:28:42 +0300

    liste visitor sur chart external url

--- src/components/ChartStatistique/ChartExternalUrl.vue
+++ src/components/ChartStatistique/ChartExternalUrl.vue
@@ -1,9 +1,94 @@
 <template>
   <h3>About external Qrcodes</h3>
-  <div v-if="hasData">
-    <div class="chart-container">
-      <apexchart type="bar" height="460" :options="chartOptions" :series="series" />
+  <div v-if="hasData" style="position: relative;">
+
+    <div v-if="showModal" class="overlay" >
+      <div class="modal" @click.stop>
+       <div>
+          <h3 class="text-lg font-bold" style="margin: -15px;">Visitors {{visitors[0].os}}</h3>
+          <button class="btn text-lg close-btn" style="color: red;" @click="showModal = false">x</button>
+       </div>
+        <div class="modal-content">
+
+
+            <el-table :data="visitors" class="custom-header" style="font-size: 13px !important;padding: 0px !important;margin: 0px !important;">
+           
+              <el-table-column label="Os" align="center" style="width: 20%; min-width: 800px;">
+                <template #default="scope">
+                  <span style=" width: 20%; min-width: 800px; word-wrap: break-word;word-break: break-word;white-space: normal">
+                    {{ scope.row.os }}
+                  </span>
+
+                </template>
+              </el-table-column>
+
+              <!-- <el-table-column label="OS" align="center" prop="os"/> -->
+
+               <el-table-column label="Device" align="center" style="width: 20%; min-width: 800px;">
+                <template #default="scope">
+                  <span style="width: 20%; min-width: 800px; word-wrap: break-word;word-break: break-word;white-space: normal">
+                    {{ scope.row.device }}
+                  </span>
+
+                </template>
+              </el-table-column>
+
+              <!-- <el-table-column label="Device" prop="device"   /> -->
+
+              <!-- <el-table-column label="Country" prop="country" /> -->
+              <el-table-column label="Country" align="center" style="width: 20%; min-width: 800px;">
+                <template #default="scope">
+                  <span style="width: 20%; min-width: 800px; word-wrap: break-word;word-break: break-word;white-space: normal">
+                    {{ scope.row.country }}
+                  </span>
+
+                </template>
+              </el-table-column>
+
+              <!-- <el-table-column label="City" prop="city"/> -->
+               <el-table-column label="City" align="center" style="width: 20%; min-width: 800px;">
+                <template #default="scope">
+                  <span style="width: 20%; min-width: 800px; word-wrap: break-word;word-break: break-word;white-space: normal">
+                    {{ scope.row.city }}
+                  </span>
+
+                </template>
+              </el-table-column>
+
+              <!-- <el-table-column label="Gps" align="center" prop="gps"  show-overflow-tooltip/> -->
+              <el-table-column label="Gps" align="center" style="width: 20%; min-width: 800px;">
+                <template #default="scope">
+                  <span style="width: 20%; min-width: 800px; word-wrap: break-word;word-break: break-word;white-space: normal">
+                    {{ scope.row.gps }}
+                  </span>
+
+                </template>
+              </el-table-column>
+             
+              <!-- <el-table-column label="Visited at" prop="created_at" /> -->
+            <el-table-column label="created_at" align="left" >
+                <template #default="scope">
+                  <span style="width: 20%; min-width: 800px; word-wrap: break-word;word-break: break-word;white-space: normal">
+                     {{ scope.row.created_at ? moment(scope.row.created_at).format('YYYY-MM-DD HH:mm') : '' }}
+
+                  </span>
+
+                </template>
+              </el-table-column>
+             
+          </el-table>
+
+          
+        </div>
+        
+      </div>
     </div>
+
+
+    <div class="chart-container">
+      <apexchart type="bar" height="460" :options="chartOptions" :series="series"  @dataPointSelection="handleBarClick"/>
+    </div>
+
   </div>
   <div v-else class="content-message">
     <div>No scan<br>
@@ -27,25 +112,58 @@ import { useRoute } from 'vue-router';
 import services from '@Services/services.js';
 import moment from 'moment';
 import { useUserStore } from "@Stores/user.js"
+import {
+    ElMessage,
+    ElTable,
+    ElTableColumn,
+    ElPopconfirm,
+    ElButton,
+    ElInput, ElOption, ElSelect, ElDatePicker, ElTooltip
+} from 'element-plus'
 
 const route = useRoute();
 const dataChart = ref([]);
 const series = ref([]);
+const visitors = ref([]);
 const category = ref([]);
 const start_date = inject('start_date');
 const end_date = inject('end_date');
 const timePeriods = inject('timePeriods');
 const establishment = inject('establishment');
 const establishments = inject('establishments');
-const staff = inject('staffFilter');
+const staff = inject('staffFilter'); 
 const units = inject('unitsFilter');
 const userStore = useUserStore();
 const hasData = ref(false);
+const showModal = ref(false);
+
+
+function interpolateColor(color1, color2, steps) {
+    const c1 = color1.match(/\w\w/g).map(c => parseInt(c, 16));
+    const c2 = color2.match(/\w\w/g).map(c => parseInt(c, 16));
+    const colors = [];
+
+    for (let i = 0; i <= steps; i++) {
+        const r = Math.round(c1[0] + (c2[0] - c1[0]) * (i / steps));
+        const g = Math.round(c1[1] + (c2[1] - c1[1]) * (i / steps));
+        const b = Math.round(c1[2] + (c2[2] - c1[2]) * (i / steps));
+        colors.push(`#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase()}`);
+    }
+    return colors;
+}
+
+
 
 const chartOptions = ref({
   chart: {
-    id: 'vuechart-example',
+    id: 'vuechart-example2',
     stacked: true,
+  },
+  events: {
+      dataPointSelection: (event, chartContext, config) => {
+       
+        handleBarClick(event, chartContext, config);
+      }
   },
   xaxis: {
     categories: [],
@@ -60,8 +178,8 @@ const chartOptions = ref({
       }
     }
   },
-  // colors: userStore.user.partner ? (userStore.user.partner.back_color == "#0a8964" ? ['#0a8964', '#48c16c'] : ['#00569D', '#009DCF']) : (userStore.user.customer.partner_back_color == "#0a8964" ? ['#0a8964', '#48c16c'] : ['#00569D', '#009DCF']),
-  colors: userStore.user.partner ? [userStore.user.partner.back_color, userStore.user.partner.title_color] : [userStore.user.customer.partner_back_color, userStore.user.customer.partner_title_color],
+  colors: interpolateColor(userStore.user.partner ? userStore.user.partner.back_color : userStore.user.customer.partner_back_color, userStore.user.partner ? userStore.user.partner.title_color : userStore.user.customer.partner_title_color, 3),
+  // colors: userStore.user.partner ? [userStore.user.partner.back_color, userStore.user.partner.title_color] : [userStore.user.customer.partner_back_color, userStore.user.customer.partner_title_color],
   legend: {
     position: 'bottom',
     horizontalAlign: 'center',
@@ -73,14 +191,25 @@ const chartOptions = ref({
   },
 });
 
-const getMaxData = (data1, data2) => {
-  let max1 = Math.ceil(Math.max(...data1) / 10) * 10;
-  let max2 = Math.ceil(Math.max(...data2) / 10) * 10;
+const handleBarClick = (event, chartContext, config)=> {
+ 
+  const { dataPointIndex } = config;
+  const serieName = series.value[config.seriesIndex]?.name;
+  
+    getVisitors(chartOptions.value.xaxis.categories[dataPointIndex], chartOptions.value.xaxis.categories[dataPointIndex], timePeriods.value, establishment.value, staff.value, units.value,serieName);
+ 
+  showModal.value = true;
+  
+}
 
-  if (max2 > max1) {
-    max1 = max2;
-  }
-  return max1;
+const getMaxData = (_series) => {
+ const maxIndex = _series[0].data.length;
+const sums = Array.from({ length: maxIndex }, (_, i) => 
+  _series.reduce((sum, s) => sum + s.data[i], 0)
+);
+
+const maxData = Math.max(...sums);
+  return maxData;
 }
 
 const IsValueOkay = (value) => (value == '' || value == 'Global' || value == 0 || value == null || value == undefined) ? false : true;
@@ -115,7 +244,7 @@ const loadData = async (start_date, end_date, timePeriods, establishment, staff,
       dataChart.value = response.data;
       category.value = response.data.categories || [];
       series.value = response.data.series || [];
-      const maxValue = response.data.series.length > 0 ? getMaxData(response.data.series[0].data, response.data.series[1].data) : 0;
+      const maxValue = response.data.series.length > 0 ? getMaxData(response.data.series) : 0;
       chartOptions.value = {
         ...chartOptions.value,
         xaxis: {
@@ -124,12 +253,49 @@ const loadData = async (start_date, end_date, timePeriods, establishment, staff,
         yaxis: {
           max: maxValue,
         },
+        colors : interpolateColor(userStore.user.partner ? userStore.user.partner.back_color : userStore.user.customer.partner_back_color, userStore.user.partner ? userStore.user.partner.title_color : userStore.user.customer.partner_title_color, response.data.series.length)
       };
       const total = series.value.reduce((totalAcc, serie) => {
         return totalAcc + (serie.data ? serie.data.reduce((acc, curr) => acc + curr, 0) : 0);
       }, 0);
+     
 
       hasData.value = total > 0;
+
+    } else {
+      console.error('Error fetching data:', response);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getVisitors = async (start_date, end_date, timePeriods, establishment, staff, units,caption) => {
+
+
+
+  let api = `/customer/visitor/external/list?tag=${route.params.tag}&from=${start_date}&to=${end_date}&caption=${caption}&type=${timePeriods || 'daily'}`
+  if (establishment) {
+    api = api + `&establishment=${establishment}`
+  }
+  if (staff) {
+    api = api + `&staff=${staff}`
+  }
+  if (units) {
+    api = api + `&units=${units}`
+  }
+
+  try {
+    const response = await new Promise((resolve) => {
+      services.get_Record(api, (response) => {
+        resolve(response);
+      });
+    });
+
+
+    if (response.status === 200) {
+      visitors.value = response.data;
+       console.log(response.data);
 
     } else {
       console.error('Error fetching data:', response);
@@ -159,6 +325,47 @@ export default {
 </script>
 
 <style scoped>
+
+  .modal {
+  position: absolute;
+  top: 0%;
+  left: -5%;
+ background: white;
+  padding: 10px;
+  border-radius: 12px;
+  box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.2);
+  width: 110%;
+  height: 90%;
+  overflow: auto; 
+  display: flex;
+  flex-direction: column;
+  z-index: 1000 !important;
+  scrollbar-width: none;
+}
+
+.modal-content {
+  flex-grow: 1;
+  overflow-x: auto; 
+  overflow-y: auto; 
+  border: 1px solid #ccc;
+  padding: 5px;
+  white-space: nowrap; 
+}
+
+.close-btn {
+  position: absolute;
+  top: 1px;
+  right: 5px;
+  background: transparent;
+  border: none;
+  font-size: 20px;
+  color: red;
+  cursor: pointer;
+
+}
+
+
+
 .chart-container {
   width: 100%;
   max-width: 100%;
